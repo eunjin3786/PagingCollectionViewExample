6ee5b01db755488eae9f8180aef73458a38ff51c
jinny <jinny.jo@kakaocorp.com>
2020-09-20 14:28:32 +0900

    코드정리.

--- CenterCollectionViewExample/ViewController.swift
+++ CenterCollectionViewExample/ViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var previousOffset: CGFloat = 0
+    private var currentPage: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,14 +15,14 @@ class ViewController: UIViewController {
     func setupCollectionView() {
         collectionView.dataSource = self
         collectionView.delegate = self
-
+        
         let collectionViewLayout: UICollectionViewFlowLayout = {
-            let layout = CustomCollectionViewFlowLayout()
+            let layout = UICollectionViewFlowLayout()
             layout.itemSize = CGSize(width: self.view.bounds.width - 40, height: self.view.bounds.height - 80)
             layout.minimumLineSpacing = 20
             layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
             layout.scrollDirection = .horizontal
-            layout.headerReferenceSize = CGSize(width: 100, height: self.view.bounds.height - 80)
+            layout.headerReferenceSize = CGSize(width: 200, height: self.view.bounds.height - 80)
             layout.footerReferenceSize = CGSize(width: 350, height: self.view.bounds.height - 80)
             return layout
         }()
@@ -32,6 +35,7 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
     }
@@ -48,41 +52,19 @@ extension ViewController: UICollectionViewDataSource {
 }
 
 extension ViewController: UICollectionViewDelegate {
-
-//    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-//        guard let layout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
-//
-//        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
-//
-//        let estimatedIndex = scrollView.contentOffset.x / cellWidthIncludingSpacing
-//        let index: Int
-//        if velocity.x > 0 {
-//            index = Int(ceil(estimatedIndex))
-//        } else if velocity.x < 0 {
-//            index = Int(floor(estimatedIndex))
-//        } else {
-//            index = Int(round(estimatedIndex))
-//        }
-//
-//        targetContentOffset.pointee = CGPoint(x: CGFloat(index) * cellWidthIncludingSpacing, y: 0)
-//    }
-}
-
-// https://stackoverflow.com/questions/23990863/uicollectionview-cell-scroll-to-centre
-class CustomCollectionViewFlowLayout: UICollectionViewFlowLayout {
-
-    private var previousOffset: CGFloat = 0
-    private var currentPage: Int = 0
     
-//    override var collectionViewContentSize: CGSize {
-//        let originalContentSize = super.collectionViewContentSize
-//        return CGSize(width: originalContentSize.width + 40, height: originalContentSize.height)
-//    }
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let point = self.targetContentOffset(scrollView, withVelocity: velocity)
+        targetContentOffset.pointee = point
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: velocity.x, options: .allowUserInteraction, animations: {
+            self.collectionView.setContentOffset(point, animated: true)
+        }, completion: nil)
+    }
     
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        guard let collectionView = collectionView else {
-            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
-        }
+    func targetContentOffset(_ scrollView: UIScrollView, withVelocity velocity: CGPoint) -> CGPoint {
+        
+        guard let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return .zero }
         
         if previousOffset > collectionView.contentOffset.x && velocity.x < 0 {
             currentPage = currentPage - 1
@@ -90,26 +72,12 @@ class CustomCollectionViewFlowLayout: UICollectionViewFlowLayout {
             currentPage = currentPage + 1
         }
         
-        let additional = itemSize.width -         headerReferenceSize.width + minimumLineSpacing
+        let additional = (flowLayout.itemSize.width + flowLayout.minimumLineSpacing) - flowLayout.headerReferenceSize.width
         
-        let updatedOffset = (itemSize.width + minimumLineSpacing) * CGFloat(currentPage) - additional
-        
-//        let maxContentOffset: CGFloat = {
-//            let totalCellCount = collectionView.numberOfItems(inSection: 0)
-//            let totalCellWidth = CGFloat(totalCellCount) * (itemSize.width + minimumLineSpacing)
-//            let headerFooterWidth = headerReferenceSize.width + footerReferenceSize.width
-//            let totalContentSize = totalCellWidth + headerFooterWidth + sectionInset.left + sectionInset.right
-//            return totalContentSize - collectionView.bounds.width - sectionInset.right - minimumLineSpacing
-//        }()
-//
-//        if updatedOffset > maxContentOffset {
-//            updatedOffset = maxContentOffset
-//        }
+        let updatedOffset = (flowLayout.itemSize.width + flowLayout.minimumLineSpacing) * CGFloat(currentPage) - additional
         
         previousOffset = updatedOffset
-//        print(updatedOffset)
-//        print("max \(maxContentOffset)")
-//
-        return CGPoint(x: updatedOffset, y: proposedContentOffset.y)
+        
+        return CGPoint(x: updatedOffset, y: 0)
     }
 }
